d9810c8a4074a84ab5b9f5f1ce37f77f3c201cb8
Jhenie <highhalma@gmail.com>
2025-11-14 14:37:24 -0300

    using random values

--- activation_functions.py
+++ activation_functions.py
@@ -1,7 +1,17 @@
 import math as m
+import random as rd
 
 def S(x, w, b):
     return (x * w) + b
 
 def sigmoid(S):
     return 1 / (1 + m.exp(-S))
+
+t = 1.00
+
+w = rd.random()
+b = rd.random()
+
+epochs = 100
+for i in range(epochs):
+    pass
