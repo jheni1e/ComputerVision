6d816e679a476c5dd9cf05fdc58893b00b35ee81
Jhenie <highhalma@gmail.com>
2025-12-03 14:46:37 -0300

    now working with any shape

--- 03. Kernel/main.py
+++ 03. Kernel/main.py
@@ -11,21 +11,23 @@ def filter_function(image: List[List[int]], kernel: List[List[int]]):
 
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
-            node1 = safe(image, i-1, j-1) * kernel[0][0]
-            node2 = safe(image, i-1, j)   * kernel[0][1]
-            node3 = safe(image, i-1, j+1) * kernel[0][2]
-            node4 = safe(image, i, j-1)   * kernel[1][0]
-            node5 = safe(image, i, j)     * kernel[1][1]
-            node6 = safe(image, i, j+1)   * kernel[1][2]
-            node7 = safe(image, i+1, j-1) * kernel[2][0]
-            node8 = safe(image, i+1, j)   * kernel[2][1]
-            node9 = safe(image, i+1, j+1) * kernel[2][2]
+            newimage = 0
+            total = 0
+
+            for k in range(len(kernel)):
+                for l in range (len(kernel[k])):
+                    if i+k >= len(image) or j+l >= len(image[0]):  #
+                        continue
+                    total += (image[i+k][j+l] * kernel[k][l])
+                    newimage += (kernel[k][l])  
+            if newimage != 0:
+                total = total / newimage
 
             ksum = sum(sum(k) for k in kernel)
             if ksum == 0:
                 ksum = 1
 
-            s = (node1 + node2 + node3 + node4 + node5 + node6 + node7 + node8 + node9) / ksum
+            s = total / ksum
             
             if s > 255:
                 s = 255
