168d9d1f885bb93833ce774dc6a5d949730429cf
Jhenie <highhalma@gmail.com>
2025-12-08 11:28:15 -0300

    testing the model i trained

--- 04. Keras/main.py
+++ 04. Keras/main.py
@@ -1,9 +1,39 @@
-from tensorflow.keras import models, layers, activations, initializers
+import cv2
+import os
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras import utils
+import PIL
+from PIL import UnidentifiedImageError
 
-model = models.Sequential([
-    layers.Input(shape=(28,28,3)), # 28x28x1
-    layers.Conv2D(10, kernel_size=(3,3), strides=(1,1), padding='same'), # 10 @ 28x28
-    layers.Flatten(), # 7840
-    layers.Dense(64, activation=activations.relu, kernel_initializer=initializers.RandomNormal()), # 64
-    layers.Dense(24, activation=activations.softmax, kernel_initializer=initializers.RandomNormal()) # 24
-])
+folder = os.path.abspath("./SimpleTest")
+
+images = [
+    os.path.join(folder, f)
+    for f in os.listdir(folder)
+]
+
+model = load_model('model.keras')
+
+count = 0
+
+for image in images:
+    try:
+        img = image
+        img = utils.load_img(img, target_size=(28, 28))
+
+        img_array = utils.img_to_array(img)
+
+        img_array = np.expand_dims(img_array, axis=0)
+
+        pred = model.predict(img_array, verbose=0)
+
+        predict_value = os.path.splitext(os.path.basename(images[np.argmax(pred)]))[0]
+        real_value = os.path.splitext(os.path.basename(image))[0]
+        print('Predict: ', predict_value, 'Real: ', real_value, '\t-> ', predict_value == real_value)
+        if (predict_value == real_value):
+            count += 1
+    except UnidentifiedImageError:
+        continue;
+
+print(f"Acertos: {count}")
